3da437e76e79e0e8abda603ae54973c122222b2c
MonicaAzzahra <monicapie07@gmail.com>
2024-03-23 21:58:56 +0700

    menambahkan folder stisla

---
+++ stisla-1-2.2.0/dist/week1/array_associative.php
@@ -0,0 +1,8 @@
+<?php
+
+$animals= ["k"=>"kucing","a"=>"ayam","b"=>"banteng","i"=>"ikan"];
+echo'ini hewan :' .$animals["b"];
+echo "<br>";
+echo'ini hewan :' .$animals["i"];
+echo"<br>";
+?>
---
+++ stisla-1-2.2.0/dist/week2/hasil.php
@@ -0,0 +1,52 @@
+
+
+<?php
+//ambil value yang diinput user dan simpan ke dalam variable
+$proses = $_POST['proses'];
+$nama = $_POST['nama'];
+$matkul = $_POST['matkul'];
+$uts = $_POST['uts'];
+$uas = $_POST['uas'];
+$praktikum = $_POST['praktikum'];
+$rata_rata= ($uts+$uas+$praktikum)/3; //menghitung rata
+$keterangan = keterangan($rata_rata);
+$grade = grade($rata_rata);
+
+
+function keterangan ($rata_rata){
+if ($rata_rata >=55 && $rata_rata<=100){
+    return 'Anda Lulus';
+}else{
+    return 'Anda Tidak Lulus';
+}
+}
+    
+function grade($rata_rata){
+    if ($rata_rata >= 85 && $rata_rata <=100) {
+        return "A (Sangat baik)";
+    } elseif ($rata_rata >= 70 && $rata_rata <=84) {
+        return "B (baik)";
+    } elseif ($rata_rata >= 56 && $rata_rata <=69) {
+        return "C (Cukup)";
+    }  elseif ($rata_rata >= 36 && $rata_rata <=55) {
+        return "D (Kurang)";
+    } elseif ($rata_rata >= 0 && $rata_rata <=35) {
+        return "E (Sangat Kurang)";
+    }else{
+        return "I (Tidak Nilai)";
+    }
+    }
+
+
+// panggil variable yang memiliki value kolom, tampilkan menggunakan echo
+echo "proses : $proses";
+echo "<br> Nama Lengkap : $nama";
+echo "<br> Mata Kuliah : $matkul";
+echo "<br> Nilai UTS: $uts";
+echo "<br> Nilai UAS : $uas";
+echo "<br>Nilai Praktikum : $praktikum";
+echo "<br>Rata-rata nilai : $rata_rata";
+echo "<br>keterangan : $keterangan";
+echo "<br>grade : $grade";
+
+?>
